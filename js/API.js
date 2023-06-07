e1ddb9f5270a9d731f77a09719c1799332ccccaf
tiocasa.py <tiocosa.py@gmail.com>
2023-06-07 00:57:35 -0300

    API y resposive en index.html & contactos.html formulario en JS

---
+++ js/API.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var weatherInfo = document.getElementById("weatherInfo");
+  
+    // Configura los parámetros de la solicitud a la API de OpenWeatherMap
+    var apiKey = "599b2da13c1cfb4a740bb61c830dcdbc";
+    var city = "Rosario";
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+    // Realiza la solicitud a la API
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        // Maneja la respuesta de la API
+        var temperature = data.main.temp;
+        var description = data.weather[0].description;
+  
+        // Muestra la información del clima en el elemento weatherInfo
+        weatherInfo.textContent = `Temperatura: ${temperature}°C, Descripción: ${description}`;
+      })
+      .catch(function(error) {
+        // Maneja los errores de la solicitud
+        weatherInfo.textContent = "Error al obtener la información del clima";
+        console.log(error);
+      });
+  });
